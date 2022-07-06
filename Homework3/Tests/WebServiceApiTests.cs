0d5a89bba1efd47207a190418d4fff60a5ea31db
Derrick Lloyd Pua <derricklloyd.pua@ph.softvision.global>
2022-07-06 23:09:20 +0800

    Finish Homework3

---
+++ Homework3/Tests/WebServiceApiTests.cs
@@ -0,0 +1,28 @@
+﻿using CognizantSoftvision.Maqs.BaseWebServiceTest;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Net;
+using System.Text;
+using WebServiceModel;
+
+namespace Tests
+{
+    /// <summary>
+    /// WithAuth test class
+    /// </summary>
+    [TestClass]
+    public class WebServiceApiTests : BaseWebServiceTest
+    {
+        [TestMethod]
+        public void Homework3()
+        {
+            this.WebServiceDriver.HttpClient.DefaultRequestHeaders.Add("Auth", "AuthKey");
+
+            ArrayOfProducts[] arrayOfProducts = this.WebServiceDriver.Get<ArrayOfProducts[]>("/api/XML_JSON/GetAllProducts", "application/json");
+            Assert.AreEqual("Tomato Soup", arrayOfProducts[0].Name, "Wrong Name");
+            Assert.AreEqual("Toys", arrayOfProducts[1].Category, "Wrong Category");
+            Assert.AreEqual(16.99, arrayOfProducts[2].Price, "Wrong Price");
+        }
+
+    }
+}
