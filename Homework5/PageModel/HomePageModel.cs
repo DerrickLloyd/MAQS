2f4994759c60eabf4ba3b48b6f241785c578446b
Derrick Lloyd Pua <derricklloyd.pua@ph.softvision.global>
2022-07-08 00:07:12 +0800

    Finish Homework5

---
+++ Homework5/PageModel/HomePageModel.cs
@@ -0,0 +1,54 @@
+﻿using CognizantSoftvision.Maqs.BaseSeleniumTest;
+using CognizantSoftvision.Maqs.BaseSeleniumTest.Extensions;
+using OpenQA.Selenium;
+
+namespace PageModel
+{
+    /// <summary>
+    /// Page object for the Automation page
+    /// </summary>
+    public class HomePageModel : BaseSeleniumPageModel
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HomePageModel" /> class.
+        /// </summary>
+        /// <param name="testObject">The selenium test object</param>
+        public HomePageModel(ISeleniumTestObject testObject) : base(testObject)
+        {
+        }
+
+        /// <summary>
+        /// Gets about button
+        /// </summary>
+        private LazyElement AboutButton
+        {
+            get { return this.GetLazyElement(By.CssSelector("#About"), "About button"); }
+        }
+
+        /// <summary>
+        /// Gets welcome message
+        /// </summary>
+        private LazyElement WelcomeMessage
+        {
+            get { return this.GetLazyElement(By.CssSelector("#WelcomeMessage"), "Welcome message"); }
+        }
+
+        /// <summary>
+        /// Check if the home page has been loaded
+        /// </summary>
+        /// <returns>True if the page was loaded</returns>
+        public override bool IsPageLoaded()
+        {
+            return this.WelcomeMessage.Displayed;
+        }
+
+        /// <summary>
+        /// Click About Button
+        /// </summary>
+        public void ClickAboutButton()
+        {
+            this.AboutButton.Click();
+        }
+    }
+}
+
---
+++ Homework5/Tests/SeleniumTestsVSUnit.cs
@@ -0,0 +1,45 @@
+﻿using CognizantSoftvision.Maqs.BaseSeleniumTest;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PageModel;
+using OpenQA.Selenium;
+
+namespace Tests
+{
+    /// <summary>
+    /// SeleniumTest test class with VS unit
+    /// </summary>
+    [TestClass]
+    public class SeleniumTestsVSUnit : BaseSeleniumTest
+    {
+        [TestMethod]
+        public void Homework5()
+        {
+            string username = "Ted";
+            string password = "123";
+
+            //Chrome
+            this.ManagerStore.AddOrOverride(new SeleniumDriverManager(() =>
+                 WebDriverFactory.GetBrowserWithDefaultConfiguration(BrowserType.Chrome), this.TestObject));
+
+            LoginPageModel page = new LoginPageModel(this.TestObject);
+            page.OpenLoginPage();
+            HomePageModel homepage = page.LoginWithValidCredentials(username, password);
+            Assert.IsTrue(homepage.IsPageLoaded());
+            homepage.ClickAboutButton();
+            AboutPageModel aboutPage = new AboutPageModel(this.TestObject);
+            Assert.IsTrue(aboutPage.IsPageLoaded());
+
+            //Firefox Override
+            this.ManagerStore.AddOrOverride(new SeleniumDriverManager(() =>
+                 WebDriverFactory.GetBrowserWithDefaultConfiguration(BrowserType.Firefox), this.TestObject));
+
+            LoginPageModel pageOverride = new LoginPageModel(this.TestObject);
+            pageOverride.OpenLoginPage();
+            HomePageModel homepageOverride = pageOverride.LoginWithValidCredentials(username, password);
+            Assert.IsTrue(homepageOverride.IsPageLoaded());
+            homepageOverride.ClickAboutButton();
+            AboutPageModel aboutPageOverride = new AboutPageModel(this.TestObject);
+            Assert.IsTrue(aboutPageOverride.IsPageLoaded());
+        }
+    }
+}
