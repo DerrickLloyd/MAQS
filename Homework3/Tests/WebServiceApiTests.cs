8e36f3d64576a275374b93dce8f04ef0f3478f9a
Derrick Lloyd Pua <derricklloyd.pua@ph.softvision.global>
2022-07-11 21:55:14 +0800

    Finished Homework3

--- Homework3/Tests/WebServiceApiTests.cs
+++ Homework3/Tests/WebServiceApiTests.cs
@@ -1,27 +1,44 @@
 ﻿using CognizantSoftvision.Maqs.BaseWebServiceTest;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
-using System.Net;
-using System.Text;
 using WebServiceModel;
+using HttpClientFactory = CognizantSoftvision.Maqs.BaseWebServiceTest.HttpClientFactory;
+using CognizantSoftvision.Maqs.Utilities.Helper;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
 
 namespace Tests
 {
-    /// <summary>
-    /// WithAuth test class
-    /// </summary>
     [TestClass]
     public class WebServiceApiTests : BaseWebServiceTest
     {
+
+        /// <summary>
+        /// Setup our webservice driver
+        /// </summary>
+        [TestInitialize]
+        public void Initialize()
+        {
+            this.ManagerStore.AddOrOverride(new WebServiceDriverManager(() =>
+            {
+                HttpClient client = HttpClientFactory.GetClient(
+                    new Uri(Config.GetValueForSection(ConfigSection.WebServiceMaqs, "WebServiceUri")), WebServiceConfig.GetWebServiceTimeout());
+                client.DefaultRequestHeaders.Authorization =
+                    new AuthenticationHeaderValue("Auth", "AuthKey");
+                return client;
+            }, this.TestObject));
+        }
+
         [TestMethod]
         public void Homework3()
         {
-            this.WebServiceDriver.HttpClient.DefaultRequestHeaders.Add("Auth", "AuthKey");
 
             ArrayOfProducts[] arrayOfProducts = this.WebServiceDriver.Get<ArrayOfProducts[]>("/api/XML_JSON/GetAllProducts", "application/json");
             Assert.AreEqual("Tomato Soup", arrayOfProducts[0].Name, "Wrong Name");
             Assert.AreEqual("Toys", arrayOfProducts[1].Category, "Wrong Category");
             Assert.AreEqual(16.99, arrayOfProducts[2].Price, "Wrong Price");
+            Assert.AreEqual(3, arrayOfProducts.Length, "Wrong Count of Products");
         }
 
     }
